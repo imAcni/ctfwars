0282e4076c514879c0325872512e4eb02cc2a0a4
imAcni <101078007+imAcni@users.noreply.github.com>
2025-02-18 23:00:40 -0500

    asda
    sdasd

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const socket = io("https://wbsocket-production.up.railway.app");  // Use your deployed WebSocket URL
 
-socket.on("connect", () => {
-    console.log("Connected to WebSocket Server:", socket.id);
-});
-
-document.getElementById("inputBox").addEventListener("input", (e) => {
-    socket.emit("playerTyped", { text: e.target.value });
-});
-
-socket.on("updateGame", (data) => {
-    console.log("Received update:", data.text); // Debugging
-    document.getElementById("opponentText").innerText = data.text;
-});
 
 document.getElementById("playButton").addEventListener("click", async () => {
     const response = await fetch("https://wbsocket-production.up.railway.app/create-lobby");
